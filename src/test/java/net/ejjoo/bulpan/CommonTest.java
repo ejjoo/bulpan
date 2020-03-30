10e6423040702ef73abd8c5dab3067e0bdf45027
Euijong Joo <ejjoo@archbears.com>
2020-03-31 03:24:16 +0900

    welcome page 추가

---
+++ src/test/java/net/ejjoo/bulpan/CommonTest.java
@@ -0,0 +1,38 @@
+package net.ejjoo.bulpan;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@WebAppConfiguration()
+@ActiveProfiles("local")
+public class CommonTest {
+	@Autowired
+	private WebApplicationContext appContext;
+
+	private MockMvc mockMvc;
+
+	@BeforeEach
+	void setup() {
+		this.mockMvc= MockMvcBuilders.webAppContextSetup(this.appContext).build();
+	}
+	@Test
+	public void welcome() throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders.get("/")
+				.accept(MediaType.APPLICATION_JSON))
+				.andDo(print())
+				.andExpect(status().isOk());
+	}
+}
