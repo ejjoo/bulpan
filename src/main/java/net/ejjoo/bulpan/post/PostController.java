7120baeec16b7a504fc5ecfaa0bc84942aa63297
Euijong Joo <ejjoo@archbears.com>
2020-04-01 23:31:06 +0900

    add list view

--- src/main/java/net/ejjoo/bulpan/post/PostController.java
+++ src/main/java/net/ejjoo/bulpan/post/PostController.java
@@ -1,10 +1,25 @@
 package net.ejjoo.bulpan.post;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.List;
-
-@RestController
+@Controller
+@RequestMapping("post")
 public class PostController {
+	public static final String VIEW_LIST = "list";
+
+	private final PostService postService;
+
+	public PostController(PostService postService) {
+		this.postService = postService;
+	}
+
+	@GetMapping("list")
+	public String list(ModelMap modelMap) {
+		modelMap.put("list", postService.getListAll());
+		return VIEW_LIST;
+	}
+
 }
