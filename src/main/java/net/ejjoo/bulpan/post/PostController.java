01eff10c59f53375cae70aa51c696ad51a7e9c81
Euijong Joo <ejjoo@archbears.com>
2020-05-27 20:00:17 +0900

    implements post form method

--- src/main/java/net/ejjoo/bulpan/post/PostController.java
+++ src/main/java/net/ejjoo/bulpan/post/PostController.java
@@ -1,19 +1,27 @@
 package net.ejjoo.bulpan.post;
 
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("post")
 public class PostController {
 	public static final String VIEW_LIST = "list";
+	public static final String VIEW_FORM = "form";
 
 	private final PostService postService;
 
 	public PostController(PostService postService) {
 		this.postService = postService;
+	}
+
+	@GetMapping("form")
+	public String form(ModelMap modelMap) {
+		return VIEW_FORM;
 	}
 
 	@GetMapping("list")
@@ -22,4 +30,9 @@ public class PostController {
 		return VIEW_LIST;
 	}
 
+	@PostMapping(consumes = {MediaType.APPLICATION_FORM_URLENCODED_VALUE})
+	public String post(Post post) {
+		postService.createPost(post);
+		return "redirect:" + VIEW_LIST;
+	}
 }
--- src/test/java/net/ejjoo/bulpan/post/PostControllerTest.java
+++ src/test/java/net/ejjoo/bulpan/post/PostControllerTest.java
@@ -1,5 +1,6 @@
 package net.ejjoo.bulpan.post;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +12,12 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -24,14 +31,49 @@ public class PostControllerTest {
 
 	private MockMvc mockMvc;
 
+	private String buildUrlEncodedFormEntity(String... params) {
+		if( (params.length % 2) > 0 ) {
+			throw new IllegalArgumentException("Need to give an even number of parameters");
+		}
+		StringBuilder result = new StringBuilder();
+		for (int i=0; i<params.length; i+=2) {
+			if( i > 0 ) {
+				result.append('&');
+			}
+			try {
+				result.
+						append(URLEncoder.encode(params[i], StandardCharsets.UTF_8.name())).
+						append('=').
+						append(URLEncoder.encode(params[i+1], StandardCharsets.UTF_8.name()));
+			}
+			catch (UnsupportedEncodingException e) {
+				throw new RuntimeException(e);
+			}
+		}
+		return result.toString();
+	}
+
 	@BeforeEach
 	void setup() {
 		this.mockMvc= MockMvcBuilders.webAppContextSetup(this.appContext).build();
 	}
+
 	@Test
 	public void welcome() throws Exception {
 		mockMvc.perform(MockMvcRequestBuilders.get("/post/list")
 				.accept(MediaType.APPLICATION_JSON))
+				.andDo(print())
+				.andExpect(status().isOk());
+	}
+
+	@Test
+	public void create() throws Exception {
+		Map<String, String> reqBody = new HashMap<>();
+		reqBody.put("title", "test");
+		reqBody.put("content", "test");
+		mockMvc.perform(MockMvcRequestBuilders.post("/post")
+				.content(buildUrlEncodedFormEntity("title", "test", "content", "test"))
+				.contentType(MediaType.APPLICATION_FORM_URLENCODED))
 				.andDo(print())
 				.andExpect(status().isOk());
 	}
