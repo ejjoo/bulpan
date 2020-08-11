f5133ce6d5f5f866e0eab26fca8d9c1d796a841f
Euijong Joo <ejjoo7994@gmail.com>
2020-08-12 00:19:15 +0900

    redirect from root to post/list

--- src/main/java/net/ejjoo/bulpan/main/WelcomeController.java
+++ src/main/java/net/ejjoo/bulpan/main/WelcomeController.java
@@ -2,11 +2,12 @@ package net.ejjoo.bulpan.main;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 public class WelcomeController {
 	@GetMapping("/")
-	public String welcome() {
-		return "welcome";
+	public RedirectView welcome() {
+		return new RedirectView("post/list");
 	}
 }
